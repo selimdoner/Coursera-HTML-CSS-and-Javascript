cb8e528bdf4589726c9eb9a70f3351c0b03e51f6
Selim Doner <s.selimdoner@gmail.com>
2022-10-24 09:37:37 +0300

    seventh

--- Module4_Solution/SpeakGoodBye.js
+++ Module4_Solution/SpeakGoodBye.js
@@ -1,9 +1,9 @@
 (function(window) {
-	var goodByeSpeaker = {};
+	var byeSpeaker = {};
 	var speakWord = "Good Bye";
-	goodByeSpeaker.speak = function(name) {
+	byeSpeaker.speak = function(name) {
     	console.log(speakWord + " " + name);
 		}
-	window.goodByeSpeaker = goodByeSpeaker;
+	window.byeSpeaker = byeSpeaker;
   }	
 )(window);
